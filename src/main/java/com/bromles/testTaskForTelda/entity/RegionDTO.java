dec07aab2f771488bb4059ac3f067d190918fd13
Bromles <alexandr.v.u@gmail.com>
2021-05-27 20:40:25 +0300

    done validation for RegionDTO

--- src/main/java/com/bromles/testTaskForTelda/entity/RegionDTO.java
+++ src/main/java/com/bromles/testTaskForTelda/entity/RegionDTO.java
@@ -2,17 +2,18 @@ package com.bromles.testTaskForTelda.entity;
 
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 public class RegionDTO {
 
-    @NotBlank
-    // TODO add pattern validation
+    @NotBlank(message = "Region code cannot be blank")
+    @Pattern(regexp = "[0-9]{3}|[0-9]{2}", message = "Region code must be 2 or 3 digits")
     public String id;
 
-    @NotBlank
+    @NotBlank(message = "Region name cannot be blank")
     public String name;
 
-    @NotBlank
+    @NotBlank(message = "Region short name cannot be blank")
     public String shortName;
 
     public RegionDTO(String id, String name, String shortName) {
