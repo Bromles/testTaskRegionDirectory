2c1c0111b0e41387afc62f54e399473b0d89f52b
Bromles <alexandr.v.u@gmail.com>
2021-05-28 23:33:59 +0300

    !UNSTABLE: autoresolving empty post request body

--- src/main/java/com/bromles/testTaskForTelda/exception/ExceptionHandler.java
+++ src/main/java/com/bromles/testTaskForTelda/exception/ExceptionHandler.java
@@ -1,18 +1,19 @@
 package com.bromles.testTaskForTelda.exception;
 
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
@@ -22,10 +23,19 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers, HttpStatus status,
                                                                   WebRequest request) {
-        return ResponseEntity.status(status).body(new ExceptionResponse(status,
-                "Invalid JSON with fields: " + ex.getBindingResult().getFieldErrors().stream()
-                        .map(FieldError::getField)
-                        .collect(Collectors.toList())));
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
     }
 
     @Override
@@ -35,13 +45,20 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
                         ex.getPropertyName(), ex.getValue(), Objects.requireNonNull(ex.getRequiredType()).getSimpleName())));
     }
 
+    // TODO add proper wrong list format exception handling
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        System.out.println("\t\tenters the 'handleHttpMessageNotReadable' handler");
         return ResponseEntity.status(status).body(new ExceptionResponse(status, "Unformed JSON in request body"));
     }
 
     @Override
     protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return ResponseEntity.status(status).body(new ExceptionResponse(status, "Handler not found"));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity.status(status).body(new ExceptionResponse(status, "Unacceptable http media type"));
     }
 }
